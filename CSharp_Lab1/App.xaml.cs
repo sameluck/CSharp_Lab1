3d0d4d722b4e47174010b0a7cc34d0a55ed1a90a
sameluck <iamsameluck@gmail.com>
2022-02-19 17:33:38 +0200

    initial commit with gitignore

---
+++ CSharp_Lab1/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharp_Lab1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ CSharp_Lab1/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace CSharp_Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+        }
+        
+        
+        
+    }
+}
